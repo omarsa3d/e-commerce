abaadbb14531088568aa3171c97e1603600edf59
omarsa3d <87918992+omarsa3d@users.noreply.github.com>
2024-11-21 02:38:55 +0200

    redux-toolkit actions & navbar badge

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,12 +5,21 @@ import App from './App.jsx'
 import { ThemeProvider } from '@mui/material/styles'
 import { CssBaseline } from '@mui/material'
 import { theme } from './theme.js'
+import { Provider } from 'react-redux'
+import { configureStore } from '@reduxjs/toolkit'
+import cartReducer from './state/index.js'
+
+const store = configureStore({
+  reducer: { cart: cartReducer }
+})
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <App />
-    </ThemeProvider>
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <App />
+      </ThemeProvider>
+    </Provider>
   </StrictMode>,
 )
--- client/src/scenes/global/Navbar.jsx
+++ client/src/scenes/global/Navbar.jsx
@@ -10,10 +10,12 @@ import {
 } from '@mui/icons-material'
 import { useNavigate } from 'react-router-dom'
 import { shades } from '../../theme'
+import { setIsCartOpen } from '../../state/index'
 
 const Navbar = () => {
-  // const navigate = useNavigate();
-  // const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const cart = useSelector((state) => state.cart.cart)
 
   return (
       <div className='navbar-outer'>
@@ -26,12 +28,25 @@ const Navbar = () => {
             <IconButton sx={{color: 'black'}}>
               <SearchOutlined />
             </IconButton>
+
             <IconButton sx={{color: 'black'}}>
               <PersonOutline />
             </IconButton>
-            <IconButton sx={{color: 'black'}}>
+
+            <Badge  badgeContent={cart.length} 
+                    color='secondary' 
+                    invisible={cart.length === 0} 
+                    sx={{'&MuiBadge-badge' : {
+                      right: 5, 
+                      top: 5, 
+                      padding: '0 4px', 
+                      height: '14px', 
+                      minWidth: '13px'}}}>
+            <IconButton sx={{color: 'black'}} onClick={() => dispatch(setIsCartOpen({}))}>
               <ShoppingBagOutlined />
             </IconButton>
+            </Badge>
+
             <IconButton sx={{color: 'black'}}>
               <MenuOutlined />
             </IconButton>
